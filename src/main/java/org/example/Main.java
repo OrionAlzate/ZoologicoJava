ae3c0f84dc9546b70d229d4bd8cf3b3951ad22c5
OrionAlzate <orion_alzate@hotmail.com>
2023-02-24 19:22:50 -0500

    clase animal con modificadores de acceso

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,29 @@
 package org.example;
 
+import org.example.clases.Familia;
+
 public class Main {
     public static void main(String[] args) {
 
+        // en el main usamos las clases que nosotros programamos
+        Integer notaFinal =5;
+        // para usar una clase debo crear un objeto de la clase
+        // un objeto en cualquier lenguaje es una VARIABLE especial
 
-        System.out.println("Hello world!");
-     //   Animal pato = new Animal();
+
+        Animal pato = new Animal();
+        // SI YA TENGO UN OBJETO LO USO PARA acceder a los atributos y metodos de una clase
+
+
+        pato.setNombre("Pablo");
+        System.out.println("Hola, mi nombre es "+pato.getNombre());
+        Familia familia = new Familia();
+        familia.nombrePapa = "Manuel";
+        System.out.println("Mi nombre es " + familia.nombrePapa+ " Y soy el papá");
+        pato.edad=10;
+
+
+
 
     }
 }
---
+++ src/main/java/org/example/clases/Familia.java
@@ -0,0 +1,42 @@
+package org.example.clases;
+
+public class Familia {
+
+    public String nombrePapa;
+    protected String nombreMama;
+    private String nombreDelHijo;
+
+    private Integer edadDelPapa;
+
+    public Familia() {
+    }
+
+    public Familia(String nombrePapa, String nombreMama, String nombreDelHijo, Integer edadDelPapa) {
+        this.nombrePapa = nombrePapa;
+        this.nombreMama = nombreMama;
+        this.nombreDelHijo = nombreDelHijo;
+        this.edadDelPapa = edadDelPapa;
+    }
+
+    public String getNombrePapa() {return nombrePapa;}
+
+    public void setNombrePapa(String nombrePapa) {
+        this.nombrePapa = nombrePapa;
+    }
+
+    public String getNombreMama() {
+        return nombreMama;
+    }
+
+    public void setNombreMama(String nombreMama) {
+        this.nombreMama = nombreMama;
+    }
+
+    public String getNombreDelHijo() {
+        return nombreDelHijo;
+    }
+
+    public void setNombreDelHijo(String nombreDelHijo) {
+        this.nombreDelHijo = nombreDelHijo;
+    }
+}
